9b7ac13fdeac4ccbd326d26cf2db019d006b08c0
millimesi <millionmesi1@gmail.com>
2023-11-06 14:12:20 +0300

    py style

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 99:
-        print("{}{}".format(int(i/10), i%10))  
+        print("{}{}".format(int(i / 10), i % 10))
     elif i < 99:
-        print("{}{}".format(int(i/10), i%10), end=", ")
+        print("{}{}".format(int(i / 10), i % 10), end=", ")
