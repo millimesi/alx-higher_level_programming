0d48123958e815cd4e18835118092ec7199e8d3b
millimesi <millionmesi1@gmail.com>
2023-11-06 13:51:59 +0300

    bin is vital

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(10):
         if ((i == 8) and (j == 9)):
-            print(f"{i}{j}")
+            print("{}{}".format(i, j))
         elif i < j:
-            print(f"{i}{j}", end=", ")
+            print("{}{}, ".format(i, j), end="")
