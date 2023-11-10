dac4cfd28ce7cbcd1e2822989978619e7335cc99
millimesi <millionmesi1@gmail.com>
2023-11-10 13:39:17 +0300

    import and module styled

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,14 @@
 import sys
 
 if __name__ == "__main__":
-    print("{}".format(len(sys.argv) -1), end=" ")
+    print("{}".format(len(sys.argv) - 1), end=" ")
     if len(sys.argv) - 1 == 1:
         print("argument", end="")
     else:
         print("arguments", end="")
-    if len(sys.argv) -1 == 0:
+    if len(sys.argv) - 1 == 0:
         print(".")
     else:
         print(":")
-    for i in range(1,len(sys.argv)):
+    for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
