203a7ba4a530e1a3c718b7063966816810cfee1e
millimesi <millionmesi1@gmail.com>
2023-11-21 09:55:56 +0300

    3-0pystayle

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,4 +7,4 @@ def safe_print_division(a, b):
     except Exception:
         return result
     finally:
-         print("Inside result: {}".format(result))
+        print("Inside result: {}".format(result))
