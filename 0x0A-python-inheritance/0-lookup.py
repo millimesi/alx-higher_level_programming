ddeb85b82142452e0f21bc6dd528e35d39908ca4
millimesi <millionmesi1@gmail.com>
2023-12-04 17:55:58 +0300

    oow checker

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""
+
+'''
 Module with look up function
-"""
+'''
 
 
 def lookup(obj):
-    """list of available attributes and methods of an object"""
+    '''list of available attributes and methods of an object'''
     return dir(obj)
