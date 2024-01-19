9d723d3dd752ba2d66868fac9af28b49df478cea
millimesi <millionmesi1@gmail.com>
2024-01-19 14:22:58 +0300

    version difference

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- because Batch 3 is the best!
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,7 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- because Batch 3 is the best!
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'User_0d_1_pwd';
+-- because Batch 3 is the best!
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
