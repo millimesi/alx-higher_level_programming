934bb709e28033184664a89a6ee3b3997d57ba55
millimesi <millionmesi1@gmail.com>
2024-03-02 15:15:08 +0300

    check the requirements seriousely

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bash
-# takes in a URL, sends a request to that URL, 
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+#!/bin/bash
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI "$1" | grep -i "content-length" | awk '{print $2}' | tr -d '\r'
