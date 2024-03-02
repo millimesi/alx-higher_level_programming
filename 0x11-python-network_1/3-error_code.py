98ae9451c8237f1409340faf67a3881f8dc904a8
millimesi <millionmesi1@gmail.com>
2024-03-02 13:00:53 +0300

    e.code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+python networking 1
+'''
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try: 
+        with urllib.request.urlopen(req) as response:
+            page = response.read()
+            print(page.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
