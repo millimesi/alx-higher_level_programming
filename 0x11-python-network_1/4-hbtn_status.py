d9407e164023b3c6e1f020c4e371792dcbfe8705
millimesi <millionmesi1@gmail.com>
2024-03-02 13:23:01 +0300

    requests module

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+python networking 1
+'''
+
+
+if __name__ == "__main__":
+    import requests
+
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:\n\t- type: {}\n\t- content: {}".format(type(req.text), req.text))
